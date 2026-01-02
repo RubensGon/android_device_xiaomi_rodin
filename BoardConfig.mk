d1225a38bdab3d26a4fb662f2d5e04f664acf828
Rubens Gonçalves <93195322+rurucansado@users.noreply.github.com>
2026-01-01 21:53:25 -0300

    arch/arm64: implement armv9-a support
    Introduces armv9-a support with a focus on enhancing power efficiency through SVE2 and architecture-specific tuning.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,9 +10,9 @@ IGNORE_PREFER32_ON_DEVICE := true
 
 # Architecture
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-2a-dotprod
+TARGET_ARCH_VARIANT := armv9-a
 TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_VARIANT := cortex-a76
+TARGET_CPU_VARIANT := generic
 
 # Virtual A/B
 AB_OTA_PARTITIONS := \
